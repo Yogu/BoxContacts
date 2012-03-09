cc3d2e0082bbaeb21d06f3719af499a5705b539c
Yogu <info@yogularm.de>
2012-03-09 13:12:20 +0100

    Initial commit

---
+++ src/de/yogularm/boxcontacts/view/ContactsActivity.java
@@ -0,0 +1,15 @@
+package de.yogularm.boxcontacts.view;
+
+import de.yogularm.fritzcontacts.R;
+import de.yogularm.fritzcontacts.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
